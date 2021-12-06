b21dae645a8fee359c31e0a31e83d4bd9296a8ab
Dillan Lopez <dillanlopez@sandiego.edu>
2021-12-06 13:30:19 -0800

    Bait class finished

---
+++ Bait.java
@@ -0,0 +1,21 @@
+public class Bait extends Object{
+    private int price;
+    private int effect;
+    public Bait(int price, int effect) {
+        this.price = price;
+        this.effect = effect;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public int getEffect() {
+        return effect;
+    }
+    public void setEffect(int effect) {
+        this.effect = effect;
+    }
+    
+}
