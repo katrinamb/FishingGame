772bdcfb52a1db79faca46eeedb3cc768e22da40
Katrina <32181018+katrinamb@users.noreply.github.com>
2021-12-08 18:27:42 -0800

    Add files via upload
    Added calculateBreak method for Rare

--- Rare.java
+++ Rare.java
@@ -1,6 +1,19 @@
+import java.util.Random;
 public class Rare extends Uncommon{
     public Rare(String rarity, String name, double size, int value, int strength) {
         super(rarity, name, size, value, strength);
     }
-    public void calculateBreak(){}
+
+    // calculates if the line breaks or not while fishing
+    public int calculateBreak(int durability){
+        Random gen = new Random();
+        int breakChance = gen.nextInt(10);
+        if (breakChance + strength > durability){
+            System.out.println("Oh snap! Your line...snapped.");
+            return 1; //the rod breaks in main
+        }
+        else{
+            return 0;//the rod does not break in main
+        }
+    }
 }
